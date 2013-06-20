53a0d6fb23221e7a195722be8aecaae6a8b49683
HannahRose <hannah.rose@spacelabs.com>
2013-06-20 14:08:10 -0700

    finally added files

---
+++ src/com/example/setapp/test/CardViewTest.java
@@ -0,0 +1,12 @@
+package com.example.setapp.test;
+
+import junit.framework.TestCase;
+
+public class CardViewTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+
+}
